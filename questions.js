80214718e6d6e946db6a492eca94affd007e94a9
miacab89 <58441831+miacab89@users.noreply.github.com>
2020-01-15 17:56:47 -0600

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+      question: 'Who released "Stronger than Pride?"',
+      answers: [
+        { text: 'Sade', correct: true },
+        { text: 'Britney Spears', correct: false },
+        { text: 'Lorde', correct: false },
+        { text: 'Galas Diamanda', correct: false},
+      ]
+    },
+    {
+      question: 'Who painted "Crucifixion of Christ?"',
+      answers: [
+        { text: 'Fracis Bacon', correct: true },
+        { text: 'Piccasso', correct: false },
+        { text: 'Monet', correct: false },
+        { text: 'Michael Angelo', correct: false }
+      ]
+    },
+    {
+      question: 'Who directed "Vertigo?"',
+      answers: [
+        { text: 'Tim Burton', correct: false },
+        { text: 'Wes Craven', correct: false },
+        { text: 'Alfred Hitchcock', correct: true },
+        { text: 'David Cronenberg', correct: false }
+      ]
+    },
+    {
+      question: 'Who claimed that Earth is not the center of the universe?',
+      answers: [
+        { text: 'Einstein', correct: false },
+        { text: 'Galileo', correct: true },
+        { text: 'Almov', correct: false},
+        { text: 'Darwin', correct: false},
+      ]
+    }
+  ]
+  
